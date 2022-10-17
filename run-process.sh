b9e9b001ac1ad5d26826d2adfe07fbd15b65d0ff
goodbai-nlp <xfbai.hk@gmail.com>
2022-10-17 16:14:08 +0800

    update default processing script

--- run-process.sh
+++ run-process.sh
@@ -1,22 +1,11 @@
-corpus=LDC2020
-corpus=LDC2015
 
-for corpus in LDC2015 LDC2017 LDC2020
-do
-	OutPath=outputs/${corpus}
-	if [ ! -d ${OutPath} ];then
-	  mkdir -p ${OutPath}
-	fi
-	for cate in train val test
-	do
-		echo "Processing ${cate}..."
-		if [ "$cate" == "train" ]; then
-			cate2="training"
-		elif [ "$cate" == "val" ]; then
-			cate2="dev"
-		else
-			cate2=$cate
-		fi
-		python read_and_process.py --config config-default.yaml --input_file data/${corpus}/amrs/split/${cate2}/*-all.txt --output_prefix ${OutPath}/$cate
-	done
-done
+corpus=LDC2020
+cate=test
+
+OutPath=outputs/${corpus}
+
+if [ ! -d ${OutPath} ];then
+  mkdir -p ${OutPath}
+fi
+
+python read_and_process.py --config config-default.yaml --input_file $1 --output_prefix ${OutPath}/$cate
